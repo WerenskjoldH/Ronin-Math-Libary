87d5fdc7bc29fa40ec1cf7fab80cb9ec4cee3714
Werenskjold <Rilliden@gmail.com>
2020-07-11 19:24:21 +0900

    Minor Reformatting

--- vector.h
+++ vector.h
@@ -6,11 +6,12 @@
 
 #include "matrix.h"
 
-namespace rn {
+namespace rn
+{
 
-	class vector3f {
+	class vector3f
+	{
 	private:
-
 	public:
 		real x;
 		real y;
@@ -35,16 +36,16 @@ namespace rn {
 		}
 
 		// Constructors
-		vector3f(real iX = 0, real iY = 0, real iZ = 0) : x(iX), y(iY), z(iZ) {};
+		vector3f(real iX = 0, real iY = 0, real iZ = 0) : x(iX), y(iY), z(iZ){};
 
 		// Deconstructor
 		~vector3f() {}
 
 		// Copy Constructor
-		vector3f(const vector3f & v) : x(v.x), y(v.y), z(v.z) {};
+		vector3f(const vector3f &v) : x(v.x), y(v.y), z(v.z){};
 
 		// Operator Overloads
-		vector3f& operator=(const vector3f & v)
+		vector3f &operator=(const vector3f &v)
 		{
 			x = v.x;
 			y = v.y;
@@ -54,7 +55,7 @@ namespace rn {
 		}
 
 		// Add components to eachother and set equal
-		void operator+=(const vector3f & v)
+		void operator+=(const vector3f &v)
 		{
 			x += v.x;
 			y += v.y;
@@ -62,7 +63,7 @@ namespace rn {
 		}
 
 		// Subtract components from eachother and set equal
-		void operator-=(const vector3f & v)
+		void operator-=(const vector3f &v)
 		{
 			x -= v.x;
 			y -= v.y;
@@ -70,7 +71,7 @@ namespace rn {
 		}
 
 		// Multiply vector by scalar and set equal
-		void operator*=(const real & s)
+		void operator*=(const real &s)
 		{
 			x *= s;
 			y *= s;
@@ -78,7 +79,7 @@ namespace rn {
 		}
 
 		// Divide vector by scalar and set equal
-		void operator/=(const real & s)
+		void operator/=(const real &s)
 		{
 			real mag = 1.f / s;
 			x = x * mag;
@@ -87,15 +88,15 @@ namespace rn {
 		}
 
 		// Vector cross product
-		void operator%=(const vector3f & v)
+		void operator%=(const vector3f &v)
 		{
 			*this = cross(v);
 		}
 
 		// Calculate scalar(dot) product and return result
-		real operator*(const vector3f & v) const
+		real operator*(const vector3f &v) const
 		{
-			return x*v.x + y*v.y + z*v.z;
+			return x * v.x + y * v.y + z * v.z;
 		}
 
 		// Divide by scalar and return result
@@ -106,7 +107,7 @@ namespace rn {
 		}
 
 		// Cross vectors and return result
-		vector3f operator%(const vector3f & v)
+		vector3f operator%(const vector3f &v)
 		{
 			return cross(v);
 		}
@@ -114,31 +115,31 @@ namespace rn {
 		// Returns scalar(dot) product
 		vector3f operator*(const real s) const
 		{
-			return vector3f(s*x, s*y, x*z);
+			return vector3f(s * x, s * y, x * z);
 		}
 
 #ifdef MATRIX_H
 		// Multiply by matrix and return result
 		vector3f operator*(matrix m) const
 		{
-			return vector3f(x*m(0, 0) + y * m(1, 0) + z * m(2, 0),
-				x*m(0, 1) + y * m(1, 1) + z * m(2, 1),
-				x*m(0, 2) + y * m(1, 2) + z * m(2, 2));
+			return vector3f(x * m(0, 0) + y * m(1, 0) + z * m(2, 0),
+							x * m(0, 1) + y * m(1, 1) + z * m(2, 1),
+							x * m(0, 2) + y * m(1, 2) + z * m(2, 2));
 		}
 #endif
 
 		// Subtract component-wise and return result
-		vector3f operator-(const vector3f & v) const
+		vector3f operator-(const vector3f &v) const
 		{
 			return vector3f(x - v.x, y - v.y, z - v.z);
 		}
 
 		// Add component-wise and return result
-		vector3f operator+(const vector3f & v) const
+		vector3f operator+(const vector3f &v) const
 		{
 			return vector3f(x + v.x, y + v.y, z + v.z);
 		}
-		
+
 		// Negate components and return result
 		vector3f operator-() const
 		{
@@ -146,7 +147,7 @@ namespace rn {
 		}
 
 		// Access components array-wise for modification
-		real& operator[](int i)
+		real &operator[](int i)
 		{
 			switch (i)
 			{
@@ -184,15 +185,15 @@ namespace rn {
 		}
 
 		// Vector cross product
-		vector3f cross(const vector3f & v) const
+		vector3f cross(const vector3f &v) const
 		{
-			return vector3f(y*v.z - z * v.y, z*v.x - x * v.z, x*v.y - y * v.x);
+			return vector3f(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
 		}
 
 		// Calculate component-wise product and return result
 		vector3f componentProduct(const vector3f &v) const
 		{
-			return vector3f(x*v.x, y*v.y, z*v.z);
+			return vector3f(x * v.x, y * v.y, z * v.z);
 		}
 
 		// Calculate the unit vector and return result
@@ -201,7 +202,7 @@ namespace rn {
 			return vector3f(*this / this->magnitude());
 		}
 
-		// Invert components 
+		// Invert components
 		void invert()
 		{
 			x *= -1;
@@ -226,7 +227,7 @@ namespace rn {
 		{
 			return x * x + y * y + z * z;
 		}
-		
+
 		// Borrowing this straight from Ian Millington
 		// Add given vector scaled by float and set equal
 		void addScaledVector(const vector3f &v, real t)
@@ -236,26 +237,26 @@ namespace rn {
 			z += v.z * t;
 		}
 
-		real angleBetween(const vector3f& v) const
+		real angleBetween(const vector3f &v) const
 		{
 			vector3f aU = this->unit();
 			vector3f bU = v.unit();
-			return acosf(aU*bU);
+			return acosf(aU * bU);
 		}
 	};
 
-	std::ostream& operator<<(std::ostream& os, const vector3f& v)
+	std::ostream &operator<<(std::ostream &os, const vector3f &v)
 	{
 		return os << v.x << " " << v.y << " " << v.z;
 	}
 
-	vector3f operator*(real s, const vector3f& v)
+	vector3f operator*(real s, const vector3f &v)
 	{
-		return vector3f(s*v.x, s*v.y, s*v.z);
+		return vector3f(s * v.x, s * v.y, s * v.z);
 	}
 
 	// Or scalar product
-	real dot(const vector3f& o, const vector3f& v)
+	real dot(const vector3f &o, const vector3f &v)
 	{
 		return o.x * v.x + o.y * v.y + o.z * v.z;
 	}
@@ -274,8 +275,9 @@ namespace rn {
 	inline vector3f randomInUnitSphere()
 	{
 		vector3f p;
-		do {
-			p = 2.0f * vector3f(rand() / (RAND_MAX + 1.0), rand() / (RAND_MAX + 1.0), rand() / (RAND_MAX + 1.0)) - vector3f(1,1,1);
+		do
+		{
+			p = 2.0f * vector3f(rand() / (RAND_MAX + 1.0), rand() / (RAND_MAX + 1.0), rand() / (RAND_MAX + 1.0)) - vector3f(1, 1, 1);
 		} while (p.squaredMagnitude() >= 1.0f);
 		return p;
 	}
@@ -295,30 +297,51 @@ namespace rn {
 	}
 #endif
 
-	class vector4f {
+	class vector4f
+	{
 	private:
-
 	public:
 		real x;
 		real y;
 		real z;
 		real w;
 
+		// Getters/Setters
+		real r() const
+		{
+			return x;
+		}
+
+		real g() const
+		{
+			return y;
+		}
+
+		real b() const
+		{
+			return z;
+		}
+
+		real a() const
+		{
+			return w;
+		}
+
 		// Outside function declerations
-		real dot(const vector4f& o, const vector4f& v);
+		real dot(const vector4f &o, const vector4f &v);
 
 		// Constructors
-		vector4f() : x(0.f), y(0.f), z(0.f), w(1.f) {};
-		vector4f(real iX = 0, real iY = 0, real iZ = 0, real iW = 1) : x(iX), y(iY), z(iZ), w(iW) {};
+		vector4f() : x(0.f), y(0.f), z(0.f), w(1.f){};
+		vector4f(real iX = 0, real iY = 0, real iZ = 0, real iW = 1) : x(iX), y(iY), z(iZ), w(iW){};
 
 		// Deconstructor
 		~vector4f() {}
 
 		// Copy Constructor
-		vector4f(const vector4f & v) : x(v.x), y(v.y), z(v.z), w(v.w) {};
+		vector4f(const vector4f &v) : x(v.x), y(v.y), z(v.z), w(v.w){};
 
 		// Operator Overloads
-		vector4f& operator=(const vector4f & v)
+		vector4f &operator=(const vector4f &v)
 		{
 			x = v.x;
 			y = v.y;
@@ -328,7 +351,7 @@ namespace rn {
 			return *this;
 		}
 
-		void operator+=(const vector4f & v)
+		void operator+=(const vector4f &v)
 		{
 			x += v.x;
 			y += v.y;
@@ -336,7 +359,7 @@ namespace rn {
 			w += v.w;
 		}
 
-		void operator-=(const vector4f & v)
+		void operator-=(const vector4f &v)
 		{
 			x -= v.x;
 			y -= v.y;
@@ -344,7 +367,7 @@ namespace rn {
 			w -= v.w;
 		}
 
-		void operator*=(const real & s)
+		void operator*=(const real &s)
 		{
 			x *= s;
 			y *= s;
@@ -352,7 +375,7 @@ namespace rn {
 			w *= s;
 		}
 
-		void operator/=(const real & s)
+		void operator/=(const real &s)
 		{
 			real mag = 1.f / s;
 			x = x * mag;
@@ -362,9 +385,9 @@ namespace rn {
 		}
 
 		// Scalar(dot) product
-		real operator*(const vector4f & v) const
+		real operator*(const vector4f &v) const
 		{
-			return x*v.x + y*v.y + z*v.z + w*v.w;
+			return x * v.x + y * v.y + z * v.z + w * v.w;
 		}
 
 		vector4f operator/(const real s) const
@@ -373,28 +396,27 @@ namespace rn {
 			return vector4f(x * mag, y * mag, z * mag, w * mag);
 		}
 
-		
 		vector4f operator*(const real s) const
 		{
-			return vector4f(s*x, s*y, x*z, w*z);
+			return vector4f(s * x, s * y, x * z, w * z);
 		}
 
 #ifdef MATRIX_H
 		vector4f operator*(matrix m) const
 		{
-			return vector4f(x*m(0, 0) + y * m(1, 0) + z * m(2, 0) + w * m(3, 0),
-				x*m(0, 1) + y * m(1, 1) + z * m(2, 1) + w * m(3, 1),
-				x*m(0, 2) + y * m(1, 2) + z * m(2, 2) + w * m(3, 2),
-				x*m(0, 3) + y * m(1, 3) + z * m(2, 3) + w * m(3, 3));
+			return vector4f(x * m(0, 0) + y * m(1, 0) + z * m(2, 0) + w * m(3, 0),
+							x * m(0, 1) + y * m(1, 1) + z * m(2, 1) + w * m(3, 1),
+							x * m(0, 2) + y * m(1, 2) + z * m(2, 2) + w * m(3, 2),
+							x * m(0, 3) + y * m(1, 3) + z * m(2, 3) + w * m(3, 3));
 		}
 #endif
 
-		vector4f operator-(const vector4f & v) const
+		vector4f operator-(const vector4f &v) const
 		{
 			return vector4f(x - v.x, y - v.y, z - v.z, w - v.w);
 		}
 
-		vector4f operator+(const vector4f & v) const
+		vector4f operator+(const vector4f &v) const
 		{
 			return vector4f(x + v.x, y + v.y, z + v.z, w + v.w);
 		}
@@ -404,7 +426,7 @@ namespace rn {
 			return vector3f(-x, -y, -z);
 		}
 
-		real& operator[](int i)
+		real &operator[](int i)
 		{
 			switch (i)
 			{
@@ -448,7 +470,7 @@ namespace rn {
 
 		vector4f componentProduct(const vector4f &v) const
 		{
-			return vector4f(x*v.x, y*v.y, z*v.z, w*v.w);
+			return vector4f(x * v.x, y * v.y, z * v.z, w * v.w);
 		}
 
 		vector4f unit()
@@ -471,7 +493,7 @@ namespace rn {
 
 		real magnitude()
 		{
-			return std::sqrtf(x*x + y * y + z * z + w * w);
+			return std::sqrtf(x * x + y * y + z * z + w * w);
 		}
 		real squaredMagnitude()
 		{
@@ -480,30 +502,30 @@ namespace rn {
 
 		// Borrowing this straight from Ian Millington
 		// Add given vector scaled by float and set equal
-		void addScaledVector(const vector4f& v, real t)
+		void addScaledVector(const vector4f &v, real t)
 		{
 			x += v.x * t;
 			y += v.y * t;
 			z += v.z * t;
 			w += v.w * t;
 		}
-};
+	};
 
-	real dot(const vector4f& o, const vector4f& v)
+	real dot(const vector4f &o, const vector4f &v)
 	{
-		return o.x*v.x + o.y*v.y + o.z*v.z + o.w*v.w;
+		return o.x * v.x + o.y * v.y + o.z * v.z + o.w * v.w;
 	}
 
-	std::ostream& operator<<(std::ostream& os, const vector4f& v)
+	std::ostream &operator<<(std::ostream &os, const vector4f &v)
 	{
 		return os << v.x << " " << v.y << " " << v.z << " " << v.w;
 	}
 
-	vector4f operator*(real s, const vector4f& v)
+	vector4f operator*(real s, const vector4f &v)
 	{
-		return vector4f(s*v.x, s*v.y, s*v.z, s*v.w);
+		return vector4f(s * v.x, s * v.y, s * v.z, s * v.w);
 	}
 
-}
+} // namespace rn
 
 #endif
